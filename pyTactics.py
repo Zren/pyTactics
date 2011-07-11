5cc6d5faccfbf2155fc57509dcedf803160c9ac1
Zren <zrenfire@gmail.com>
2011-07-11 18:57:49 -0400

    Firsties

---
+++ pyTactics.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        pyTactics
+#              A python attempt at a FF Tactics-esque game.
+#
+# Author:      Chris Holland (Zren)
+#
+# Created:     11/07/2011
+# Copyright:   (c) Chris Holland 2011
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
